8bf2393da2890148f6146b65f6c4e05562512a8d
Ezra Alvarion <ezraalvarion71@gmail.com>
2023-12-18 10:44:14 +0700

    refactored file structure

--- day2/2-2.c
+++ day2/2-2.c
@@ -1,5 +1,5 @@
 /*
-    This program acts as a solution to the day 2 part 1 of AoC 2023
+    This program acts as a solution to the day 2 part 2 of AoC 2023
     Copyright (C) 2023 by Ezra Alvarion
 
     This program is free software: you can redistribute it and/or modify
@@ -21,33 +21,42 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
-#include <stdbool.h>
 #include <ctype.h>
 
-bool process_line(const char *str, const size_t len)
+uint_fast32_t process_line(const char *str, const size_t len, const int deb)
 {
+    uint_fast32_t r=0, g=0, b=0, s=0;
+    char k = 0;
     const char *mstr = strchr(str, ':') + 1;
     for (size_t i = 0; i < len; ++i) {
         if (isdigit(mstr[i])) {
-            if (mstr[i+1] == ' ' || mstr[i+1] <= '2'
-            || (mstr[i+1] <= '4' && mstr[i+3] == 'b')
-            || (mstr[i+1] <= '3' && mstr[i+3] == 'g')){
-                i += 2;
-                continue;
+            k = mstr[i+1] == ' ' ? mstr[i+2] : mstr[i+3];
+            switch(k){
+            case 'r':
+                r = r < (s = atoi(mstr+i)) ? s : r;
+                break;
+            case 'g':
+                g = g < (s = atoi(mstr+i)) ? s : g;
+                break;
+            case 'b':
+                b = b < (s = atoi(mstr+i)) ? s : b;
+                break;
+            default:
+                break; 
             }
-            else return false;
         }
     }
-    return true;
+    printf("%d: %lu, %lu, %lu\n", deb, r,g,b);
+    return r*g*b;
 }
 
-int main()
+int main(void)
 {
     char str[256] = {""};
     uint_fast32_t total = 0;
     FILE* fp = fopen("../inputs/input2.txt", "r");
     for (int i = 1; fgets(str, sizeof(str), fp) != NULL; ++i) {
-        total += process_line(str, strnlen(str, sizeof(str))) ? i : 0;
+        total += process_line(str, strlen(str), i);
         memset(str, 0, sizeof(str));
         //printf("%s\n", str);
     }
